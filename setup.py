513cfe3c515a333d9fe6bde897f6feb64d100334
César Bustíos <cesarbustios@gmail.com>
2013-01-23 09:36:12 -0500

    v0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='WorkingHours',
-    version='0.1.1',
+    version='0.1.2',
     author='César Bustíos',
     author_email='cesarbustios@gmail.com',
     url='http://pypi.python.org/pypi/WorkingHours/',
